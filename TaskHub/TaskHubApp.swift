2737b67c84ef5c3c033391bbf1b9b8ae3842508c
Alexey Tsutsoev <alexeytsuts@gmail.com>
2023-07-15 17:18:54 +0200

    Initial Commit

---
+++ TaskHub/TaskHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskHubApp.swift
+//  TaskHub
+//
+//  Created by Alexey Tsutsoev on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
